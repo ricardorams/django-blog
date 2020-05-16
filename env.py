2e12fc38b9dc697c1369388d50cf95424b5da455
Ricardo Santos <ricardo.ams@hotmail.com>
2020-05-16 20:32:32 +0000

    heroku deployment

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault("SECRET_KEY","'il-buc!mn7(55jb7rgj2qrr_mog-*=y%(q*ooo0%ojv2f4x-0@'")
+
+os.environ.setdefault("DATABASE_URL", "postgres://cixxfiwrbtbidc:2beb5a61b24dbc19693e232890a72971240bdfc7589de191b897f3bc2faf84df@ec2-176-34-97-213.eu-west-1.compute.amazonaws.com:5432/d31ju7r1hppnb6")
